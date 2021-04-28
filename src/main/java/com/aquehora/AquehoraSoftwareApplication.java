dda82238a88f191bb35920314429ca8d144fbe39
aelvismorales <favtio123@gmail.com>
2021-04-28 17:44:01 -0500

    Correcion de creacion del proyecto

---
+++ src/main/java/com/aquehora/AquehoraSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.aquehora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquehoraSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquehoraSoftwareApplication.class, args);
+    }
+
+}
--- src/test/java/com/quehora/aquehoraApiApplicationTest.java
+++
@@ -1,6 +0,0 @@
-package com.quehora;
-
-public class aquehoraApiApplicationTest
-{
-
-}

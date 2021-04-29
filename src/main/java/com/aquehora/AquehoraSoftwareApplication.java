b9526c0c4262aed4b75ae6c0aa6e0dacabea5e32
aelvismorales <favtio123@gmail.com>
2021-04-28 19:43:33 -0500

    Creacion de Entidad Nota y Usuario, agregandolo a una base de datos.

--- src/main/java/com/aquehora/AquehoraSoftwareApplication.java
+++ src/main/java/com/aquehora/AquehoraSoftwareApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Realiza toda la carga de la configuracion del proyecto
+
 public class AquehoraSoftwareApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/aquehora/entities/Nota.java
@@ -0,0 +1,61 @@
+package com.aquehora.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="notas",
+        uniqueConstraints = {
+                @UniqueConstraint(name="nota_name_unique",columnNames="name_nota")
+        })
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Nota
+{
+    @Id
+    @SequenceGenerator(name="nota_sequence",sequenceName = "nota_sequence",
+    allocationSize = 1)
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "nota_sequence")
+
+    @Column(name="id",updatable = false)
+    private Long id;
+
+    @Column(name="name_nota",nullable = false,columnDefinition = "TEXT")
+    private String name_nota;
+
+    @Column(
+            name = "importancia",
+            nullable = false,
+            columnDefinition = "INTEGER"
+    )
+    private Integer importancia;
+
+    @Column(
+            name = "contenido",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String contenido;
+    @Column(
+            name = "fecha_creacion",
+            nullable = false,
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
+    )
+    private LocalDateTime fecha_creacion;
+    @ManyToOne
+    @JoinColumn(
+            name="usuario_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name="usuario_note_fk"
+            )
+    )
+    private Usuario usuario;
+}

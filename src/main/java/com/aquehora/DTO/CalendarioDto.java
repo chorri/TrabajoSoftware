a0301b7e27a5e0fbd803070d8b247ac899277e96
AldoGomezz <aldojhairgomez@gmail.com>
2021-05-02 14:30:09 -0500

    CalendarioDto

---
+++ src/main/java/com/aquehora/DTO/CalendarioDto.java
@@ -0,0 +1,12 @@
+package com.aquehora.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class CalendarioDto {
+    private Long id;
+    private String name_calendario;
+}

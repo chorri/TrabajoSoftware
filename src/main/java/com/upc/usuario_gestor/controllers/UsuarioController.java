e1e8214af314c6dba5c48853ab2b3c96d67f214b
AldoGomezz <aldojhairgomez@gmail.com>
2021-06-27 17:03:30 -0500

    Cambio de nombre de la app

--- src/main/java/com/upc/usuario_gestor/controllers/UsuarioController.java
+++ src/main/java/com/upc/usuario_gestor/controllers/UsuarioController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/WhatTimeUsuario")
+@RequestMapping(path = "/Aquehora")
 public class UsuarioController
 {
     @Autowired
